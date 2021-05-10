bf8cc12ed780952bfad048b4ad9d946617e4bb04
setron34 <setron34@gmail.com>
2021-05-10 21:40:02 +0300

    implemented Keccak and SHA3 algorithms

--- cryptos/cryptos.cpp
+++ cryptos/cryptos.cpp
@@ -1,19 +1,6 @@
-#include "MD5.cpp"
-#include "SHA1.cpp"
-#include "SHA256.cpp"
-#include "SHA512.cpp"
-#include <iostream>
-#include <string>
-#include <cstdint>
-#include <stdint.h>
+#include "SHA3.cpp"
+
 int main()
 {
-	std::string s;
-	std::getline(std::cin, s);
-	size_t n = s.length();
-	char* input = &s[0];
-	char* output = SHA512::Evaluate(input, n);
-	for (int i = 0; i < 128; ++i) {
-		std::cout << output[i];
-	}
+	std::cout << SHA3::SHA3_512("1123");
 }
